727a87ca76ecd6e413c89292e9281a600f21284a
NguyenQuangTrung19 <nqt123456123@gmail.com>
2026-02-13 23:10:03 +0700

    má ơi huhu

--- backend/src/create-app.ts
+++ backend/src/create-app.ts
@@ -3,8 +3,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 export async function createApp() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
